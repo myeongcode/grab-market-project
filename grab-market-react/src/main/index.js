0ac20ac299531e739214cfb680322d54e782f74f
myeongcode <woomg427@yonsei.ac.kr>
2021-08-02 22:07:02 +0900

    Grab-market React2

--- grab-market-react/src/main/index.js
+++ grab-market-react/src/main/index.js
@@ -1,6 +1,23 @@
+import React from "react";
 import "./index.css";
+import axios from "axios";
 
 function MainPage() {
+  const [products, setProducts] = React.useState([]);
+  React.useEffect(function () {
+    axios
+      .get(
+        "https://00a561b1-8e76-44b3-b0d4-40007158efe9.mock.pstmn.io/products"
+      )
+      .then(function (result) {
+        const products = result.data.products;
+        setProducts(products);
+      })
+      .catch(function (error) {
+        console.error("에러 발생 : ", error);
+      });
+  }, []);
+
   return (
     <div>
       <div id="header">
@@ -13,7 +30,28 @@ function MainPage() {
           <img src="images/banners/banner1.png" />
         </div>
         <h1>판매되는 상품들</h1>
-        <div id="product-list"></div>
+        <div id="product-list">
+          {products.map(function (product, index) {
+            return (
+              <div className="product-card">
+                <div>
+                  <img className="product-img" src={product.imageUrl} />
+                </div>
+                <div className="product-contents">
+                  <span className="product-name">{product.name}</span>
+                  <span className="product-price">{product.price}</span>
+                  <div className="product-seller">
+                    <img
+                      className="product-avatar"
+                      src="images/icons/avatar.png"
+                    />
+                    <span>{product.seller}</span>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
       <div id="footer"></div>
     </div>
